1023af1e1e48caa60bba201254b810ad38e43a1e
HubbyBlue <nitrogendioxidev3@gmail.com>
2025-02-21 19:29:27 -0500

    Update layer2bot.lua

--- layer2bot.lua
+++ layer2bot.lua
@@ -24,7 +24,7 @@ if game.PlaceId == 4111023553 then
 elseif game.PlaceId == 8668476218 then 
     wait(5)
     mb_1()
-    wait(3)
+    repeat wait() until game.Players.LocalPlayer.Character:IsLoaded()
     repeat wait() print("Current HP:", game.Players.LocalPlayer.Character.Humanoid.Health / game.Players.LocalPlayer.Character.Humanoid.MaxHealth) until game.Players.LocalPlayer.Character.Humanoid.Health / game.Players.LocalPlayer.Character.Humanoid.MaxHealth > 0.85
 end
 
